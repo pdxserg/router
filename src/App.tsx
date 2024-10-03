91c66c5deb35f30933e184775544753d21195726
SergeyStarchenko <pdxserg@gmail.com>
2024-10-03 09:09:15 -0700

    button 'main page'

--- src/App.tsx
+++ src/App.tsx
@@ -5,51 +5,57 @@ import {S} from './components/pages/_styles'
 
 
 const PATH = {
-    PAGE1: '/adidas',
-    PAGE2: '/puma',
-    PAGE3: '/abibas',
-    PRICES: '/prices',
-    PAGE: '/page!',
-    // ERROR: '/error'
+	PAGE1: '/adidas',
+	PAGE2: '/puma',
+	PAGE3: '/abibas',
+	PRICES: '/prices',
+	PAGE: '/page!',
+	// ERROR: '/error'
 
 } as const
 
 function App() {
-    return (
-        <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    <S.NavWrapper><NavLink to={PATH.PAGE1}>Adidas</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE3}>Abibas</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PRICES}>Цены для оптовиков</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE}>Protected page</NavLink></S.NavWrapper>
-                    {/*<S.NavWrapper><NavLink to={PATH.ERROR}>Error page</NavLink></S.NavWrapper>*/}
-                </div>
-                <div className={styles.content}>
-                    <Outlet />
-                    {/*<Routes>*/}
-                    {/*    <Route path={'/'} element={<Navigate to={PATH.PAGE1}/>}/>*/}
+	return (
+		<div>
+			<div className={styles.header}><h1>HEADER</h1></div>
+			<div className={styles.body}>
+				<div className={styles.nav}>
+					<S.NavWrapper><NavLink to={PATH.PAGE1}>Adidas</NavLink></S.NavWrapper>
+					<S.NavWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NavWrapper>
+					<S.NavWrapper><NavLink to={PATH.PAGE3}>Abibas</NavLink></S.NavWrapper>
+					<S.NavWrapper><NavLink to={PATH.PRICES}>Цены для оптовиков</NavLink></S.NavWrapper>
+					<S.NavWrapper><NavLink to={PATH.PAGE}>Protected page</NavLink></S.NavWrapper>
+					{/*<S.NavWrapper><NavLink to={PATH.ERROR}>Error page</NavLink></S.NavWrapper>*/}
 
-                    {/*    <Route path={PATH.PAGE1} element={<Adidas/>}/>*/}
-                    {/*    <Route path={PATH.PAGE2} element={<Puma/>}/>*/}
-                    {/*    <Route path={PATH.PAGE3} element={<Abibas/>}/>*/}
-                    {/*    <Route path={PATH.PRICES} element={<Prices/>}/>*/}
-                    {/*    /!*<Route path={'/adidas/:id'} element={<Model/>}/>*!/*/}
-                    {/*    <Route path={'/:model/:id'} element={<Model/>}/>*/}
+				</div>
+				<div className={styles.content}>
+					<div className={styles.HorizontalNavigation}>
+						<a  className={styles.LinkLikeButton} href={PATH.PAGE1}>main page</a>
+						<a  className={styles.ButtonLikeLink} href={PATH.PAGE1}>Back</a>
+					</div>
 
-                    {/*    <Route path={'/*'} element={<Error404/>}/>*/}
+					<Outlet/>
+					{/*<Routes>*/}
+					{/*    <Route path={'/'} element={<Navigate to={PATH.PAGE1}/>}/>*/}
 
-                    {/*    /!*<Route path={'/page/error'} element={<Error404/>}/>*!/*/}
-                    {/*    /!*<Route path={'/*'} element={<Navigate to={'/page/error'}/>}/>*!/*/}
-                    {/*</Routes>*/}
+					{/*    <Route path={PATH.PAGE1} element={<Adidas/>}/>*/}
+					{/*    <Route path={PATH.PAGE2} element={<Puma/>}/>*/}
+					{/*    <Route path={PATH.PAGE3} element={<Abibas/>}/>*/}
+					{/*    <Route path={PATH.PRICES} element={<Prices/>}/>*/}
+					{/*    /!*<Route path={'/adidas/:id'} element={<Model/>}/>*!/*/}
+					{/*    <Route path={'/:model/:id'} element={<Model/>}/>*/}
 
-                </div>
-            </div>
-            <div className={styles.footer}>abibas 2023</div>
-        </div>
-    );
+					{/*    <Route path={'/*'} element={<Error404/>}/>*/}
+
+					{/*    /!*<Route path={'/page/error'} element={<Error404/>}/>*!/*/}
+					{/*    /!*<Route path={'/*'} element={<Navigate to={'/page/error'}/>}/>*!/*/}
+					{/*</Routes>*/}
+
+				</div>
+			</div>
+			<div className={styles.footer}>abibas 2023</div>
+		</div>
+	);
 }
 
 export default App;
